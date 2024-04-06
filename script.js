5fb18a9e5e4265c23d9c449cd8d8054209317373
Tan <tan10tvo@gmail.com>
2024-04-06 10:43:13 -0600

    javascript code

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //console.log('hello')
 
+const input = document.getElementById('input1');
+
+const list = document.getElementById('list1');
+
+function addToList() {
+    const listText = input.value.trim();
+    if (listText !== ''){
+        const li = document.createElement('li');
+        li.textContent = listText;
+        list.appendChild(li);
+        input.value = '';
+    }
+}
