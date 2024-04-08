b5c16bd37da782554d96ddb194e9532a4fa185d4
Tan <tan10tvo@gmail.com>
2024-04-08 12:27:03 -0600

    adding more code

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 //console.log('hello')
 
-const input = document.getElementById('input1');
+const input1 = document.getElementById('input1');
 
-const list = document.getElementById('list1');
+const thelist = document.getElementById('theList');
 
 function addToList() {
-    const listText = input.value.trim();
+    const listText = input1.value.trim();
     if (listText !== ''){
         const li = document.createElement('li');
+       li.addEventlistener('click', completeList);
         li.textContent = listText;
-        list.appendChild(li);
-        input.value = '';
+        thelist.appendChild(li);
+        input1.value = '';
+        
     }
+
 }
+
+function completeList(event){
+    const assignT = event.target;
+    assignT.classList.toggle('completed');
+  
+}
+
+
+
+function deleteList(event){
+    const assignT = event.target.parentElement;
+    list.removeChild(assignT);
+ 
+}
+
+   const deleteBtn = document.createElement('button');
+   deleteBtn.textContent = "Delete";
+deleteBtn.addEventListener('click', deleteTask);
+li.appendChild(deleteBtn);
